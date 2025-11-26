d04f693be8e89f7f49787f4b9b2199dd3c3c0acc
SagiKPJH1021 <Juhyung1021@gmail.com>
2025-11-26 20:13:07 +0900

    chore: add initialize

---
+++ TrainingRsultBoard/Run/ViewModels/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using DevExpress.Mvvm.CodeGenerators;
+
+namespace Run.ViewModels
+{
+    [GenerateViewModel]
+    public partial class MainViewModel
+    {
+        [GenerateProperty]
+        string _Status;
+        [GenerateProperty]
+        string _UserName;
+
+        [GenerateCommand]
+        void Login() => Status = "User: " + UserName;
+        bool CanLogin() => !string.IsNullOrEmpty(UserName);
+    }
+}
